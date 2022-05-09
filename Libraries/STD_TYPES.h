bce690d297ffcc55ba63a1cd09daa3dfd3ee7036
amr12601 <62963089+amr12601@users.noreply.github.com>
2022-05-09 16:26:47 +0200

    Edited By Amr Taha

--- Libraries/STD_TYPES.h
+++ Libraries/STD_TYPES.h
@@ -6,10 +6,10 @@ typedef unsigned short int uint_16;
 typedef unsigned int uint_32;
 typedef unsigned long long int uint_64;
 
-typedef  signed char sint_8;
-typedef  signed short int sint_16;
-typedef  signed int sint_32;
-typedef  signed long long int sint_64;
+typedef signed char sint_8;
+typedef signed short int sint_16;
+typedef signed int sint_32;
+typedef signed long long int sint_64;
 
 typedef float f32;
 typedef double f64;
