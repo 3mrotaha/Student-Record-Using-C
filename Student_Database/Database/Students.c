1e6166ca0747a20b729e20f646d73839be2d5345
amr12601 <62963089+amr12601@users.noreply.github.com>
2022-05-09 16:29:17 +0200

    Edited By Amr Taha

---
+++ Student_Database/Database/Students.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "../../Libraries/STD_TYPES.h"
+#include "students.h"
+
+
+Student_t Database_of_Students[STUDENT_NUM] = {
+	// full namee       ID     gender   age     grades
+	{"Ahmed Khalid", "1002321", "Male", 19, {15, 20, 13, 19, 16}},
+	{"Ahmed Khalid", "1002321", "Male", 19, {15, 20, 13, 19, 16}},
+	{"Ahmed Khalid", "1002321", "Male", 19, {15, 20, 13, 19, 16}},
+	{"Ahmed Khalid", "1002321", "Male", 19, {15, 20, 13, 19, 16}},
+	{"Ahmed Khalid", "1002321", "Male", 19, {15, 20, 13, 19, 16}},
+	
+	{"Ahmed Khalid", "1002321", "Male", 19, {15, 20, 13, 19, 16}},
+	{"Ahmed Khalid", "1002321", "Male", 19, {15, 20, 13, 19, 16}},
+	{"Ahmed Khalid", "1002321", "Male", 19, {15, 20, 13, 19, 16}},
+	{"Ahmed Khalid", "1002321", "Male", 19, {15, 20, 13, 19, 16}},
+	{"Ahmed Khalid", "1002321", "Male", 19, {15, 20, 13, 19, 16}},
+	
+	{"Ahmed Khalid", "1002321", "Male", 19, {15, 20, 13, 19, 16}},
+	{"Ahmed Khalid", "1002321", "Male", 19, {15, 20, 13, 19, 16}},
+	{"Ahmed Khalid", "1002321", "Male", 19, {15, 20, 13, 19, 16}},
+	{"Ahmed Khalid", "1002321", "Male", 19, {15, 20, 13, 19, 16}},
+	{"Ahmed Khalid", "1002321", "Male", 19, {15, 20, 13, 19, 16}},
+	
+	{"Ahmed Khalid", "1002321", "Male", 19, {15, 20, 13, 19, 16}},
+	{"Ahmed Khalid", "1002321", "Male", 19, {15, 20, 13, 19, 16}},
+	{"Ahmed Khalid", "1002321", "Male", 19, {15, 20, 13, 19, 16}},
+	{"Ahmed Khalid", "1002321", "Male", 19, {15, 20, 13, 19, 16}},
+	{"Ahmed Khalid", "1002321", "Male", 19, {15, 20, 13, 19, 16}},
+}
