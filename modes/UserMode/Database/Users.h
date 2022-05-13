4767148150a284e281c4f9b5d0bc0acdd6c87652
amr12601 <62963089+amr12601@users.noreply.github.com>
2022-05-13 12:41:28 +0200

    Beginning User Mode

---
+++ modes/UserMode/Database/Users.h
@@ -0,0 +1,20 @@
+#ifndef	_USER_MODULE_
+#define _USER_MODULE_
+
+/************************************************************
+ * Name :           Std_uint32UploadDatabase                *
+ * return type :    unsigned int                            *
+ * parameters :     void                                    *
+ * functionality :  allocatting a memory and upload the DB  *
+ *                  to that memory                          *
+ ************************************************************/
+uint_32 User_uint32ViewStdRec(void);
+
+
+uint_32 User_uint32EditUserName(void);
+
+
+uint_32 User_uint32EditPassword(void);
+
+
+#endif
